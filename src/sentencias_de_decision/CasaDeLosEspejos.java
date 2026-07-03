531ac6fdcec48d7eb788948819c9e693f7143943
KIKE <Enriquedlg005.dev@gmail.com>
2026-07-03 17:09:20 -0500

    logica inversa y operadores - casa de los espejos actualizado

--- src/sentencias_de_decision/CasaDeLosEspejos.java
+++ src/sentencias_de_decision/CasaDeLosEspejos.java
@@ -1,4 +1,23 @@
 package sentencias_de_decision;
 
+import java.util.Scanner;
+
 public class CasaDeLosEspejos {
+    public static void main(String[] args) {
+        System.out.println("bienvenido a la casa de los espejos");
+
+        var consola = new Scanner(System.in);
+
+        System.out.print("Cuantos años tienes? ");
+        var edad = Integer.parseInt(consola.nextLine());
+
+        System.out.println("Tienes miedo a la oscuridad?");
+        var tieneMiedo = Boolean.parseBoolean(consola.nextLine());
+
+        if(!tieneMiedo && edad > 10){
+            System.out.println("Puedes ingresar");
+        }else{
+            System.out.println("Retirese");
+        }
+    }
 }
--- src/sentencias_de_decision/SistemaBancario.java
+++ src/sentencias_de_decision/SistemaBancario.java
@@ -1,4 +1,22 @@
 package sentencias_de_decision;
 
+import java.util.Scanner;
+
 public class SistemaBancario {
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos al sistema bancario");
+
+        var consola = new Scanner(System.in);
+
+        System.out.println("Deseas salir del sistema? true/false");
+        var salirSistema = Boolean.parseBoolean(consola.nextLine());
+
+        // verificamos aplicando logica inversa
+
+        if (!salirSistema){
+            System.out.println("continuamos dentro del sistema");
+        }else{
+            System.out.println("saliendo del sistema");
+        }
+    }
 }
