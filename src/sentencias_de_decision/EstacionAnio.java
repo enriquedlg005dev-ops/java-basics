c610547e838073165bd59220cfbc7989d0dae7f4
KIKE <Enriquedlg005.dev@gmail.com>
2026-07-05 23:18:09 -0500

    estacion de años actualizada con salida - sout

--- src/sentencias_de_decision/EstacionAnio.java
+++ src/sentencias_de_decision/EstacionAnio.java
@@ -1,4 +1,27 @@
 package sentencias_de_decision;
 
+import java.util.Scanner;
+
 public class EstacionAnio {
+    public static void main(String[] args) {
+        System.out.println("estacion del año");
+
+        var consola = new Scanner(System.in);
+
+        System.out.println("proporciona el valor del mes (1-12):");
+        var mes = Integer.parseInt(consola.nextLine());
+        var estacion = "";
+        if (mes == 1 || mes == 2 || mes == 12)
+            estacion = "Invierno";
+        else if (mes == 3 || mes == 4 || mes == 5)
+            estacion = "Primavera";
+        else if (mes == 6 || mes == 7 || mes == 8)
+            estacion = "Verano";
+        else if (mes == 9 || mes == 10 || mes == 11)
+            estacion = "Otoño";
+        else
+            estacion = "estacion no reconocidad";
+
+        System.out.printf("La estacion para el mes de %d, es %s", mes, estacion);
+    }
 }
