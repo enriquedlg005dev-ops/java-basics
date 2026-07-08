4c3d2a49dcd3a8b6f33d23034837804ac3b5ec97
enrique <enriquedlg005.dev@gmail.com>
2026-07-07 20:39:28 -0500

    sistema de calificaciones - else final agregado

--- src/sentencias_de_decision/SistemaCalificaciones.java
+++ src/sentencias_de_decision/SistemaCalificaciones.java
@@ -1,5 +1,27 @@
 package sentencias_de_decision;
 
-public class SistemaCalificaciones {
+import java.util.Scanner;
 
+public class SistemaCalificaciones {
+	public static void main(String[] args) {
+		System.out.println("SISTEMA DE CALIFICACIONES");
+		var consola = new Scanner(System.in);
+		
+		var nota = Double.parseDouble(consola.nextLine());
+		var calificacion = "";
+		
+		if (nota >= 9 && nota <= 10) {
+			calificacion = "A";
+		}else if(nota >= 8 && nota < 9) {
+			calificacion = "B";
+		}else if(nota >= 7 && nota < 8) {
+			calificacion = "C";
+		}else if(nota >= 6 && nota < 7) {
+			calificacion = "D";
+		}else if(nota >= 0 && nota < 6) {
+			calificacion = "F";
+		}else {
+			calificacion = "Nota desconocida";
+		}
+	}
 }
